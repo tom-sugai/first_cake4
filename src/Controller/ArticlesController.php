462303ba11919bcdcafe26c264057b9a0b250179
Tom Sugai <tom.sugai@gmail.com>
2023-09-06 16:02:37 +0900

    create Controller Articles

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+//use App\Controller\AppController;
+
+class ArticlesController extends AppController
+{
+    public function index()
+    {
+        $this->loadComponente(Paginator); 
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));        
+    }
+}
