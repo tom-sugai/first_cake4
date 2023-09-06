3400e4764964ec1fdefc3bc63e7d2c984aeb48ea
Tom Sugai <tom.sugai@gmail.com>
2023-09-06 16:28:52 +0900

    correct spelling miss loadComponent

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Controller;
 
-//use App\Controller\AppController;
+use App\Controller\AppController;
 
 class ArticlesController extends AppController
 {
     public function index()
     {
-        $this->loadComponente('Paginator'); 
+        $this->loadComponent('Paginator'); 
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));        
     }
