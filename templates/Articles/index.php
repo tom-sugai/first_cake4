5e919406c1d9df0f0899dd39ef4b3e0ed76cec84
Tom Sugai <tom.sugai@gmail.com>
2023-09-06 16:35:31 +0900

    add tr tag for list

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -5,11 +5,13 @@
         <th>created</th>
     </tr>
     <?php foreach ($articles as $article ): ?>
-        <td>
-            <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
-        </td>
-        <td>
-            <?= $article->created->format(DATE_RFC850) ?>
-        </td>
+        <tr>
+            <td>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+            </td>
+            <td>
+                <?= $article->created->format(DATE_RFC850) ?>
+            </td>
+        </tr>
     <?php endforeach; ?>
 </table>
