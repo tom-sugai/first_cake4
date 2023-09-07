c7d86ceefbaff8adf120096429ed456d6434d5e3
Tom Sugai <tom.sugai@gmail.com>
2023-09-07 16:18:49 +0900

    add pagination

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -15,3 +15,12 @@
         </tr>
     <?php endforeach; ?>
 </table>
+<div class="paginator">
+        <ul class="pagination">
+            <?= $this->Paginator->first('<< ' . __('first')) ?>
+            <?= $this->Paginator->prev('< ' . __('previous')) ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next(__('next') . ' >') ?>
+            <?= $this->Paginator->last(__('last') . ' >>') ?>
+        </ul>
+</div>
