5c59a4d9c1558a65ed9d3bf077f600713774afdd
Tom Sugai <tom.sugai@gmail.com>
2023-09-08 11:55:48 +0900

    made add.php

---
+++ templates/Articles/add.php
@@ -0,0 +1,17 @@
+<div class="articles form large-9 medium-8 columns content">
+    <?= $this->Form->create($article) ?>
+    <fieldset>
+        <legend><?= __('Add Article') ?></legend>
+        <?php
+            //echo $this->Form->control('user_id', ['options' => $users]);
+            //echo $this->Form->text('user_id');
+            echo $this->Form->control('title');
+            echo $this->Form->control('body');
+            echo $this->Form->control('published');           
+            //echo $this->Form->control('tags._ids', ['options' => $tags]);
+            echo $this->Form->control('tag_string', ['type' => 'text']);
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->end() ?>
+</div>
