cc131e4a74a336a5014c0c4571cae0fbe9b6646e
Tom Sugai <tom.sugai@gmail.com>
2023-09-08 12:24:16 +0900

    adjust Form->controle

--- templates/Articles/add.php
+++ templates/Articles/add.php
@@ -4,12 +4,12 @@
         <legend><?= __('Add Article') ?></legend>
         <?php
             //echo $this->Form->control('user_id', ['options' => $users]);
-            //echo $this->Form->text('user_id');
+            echo $this->Form->controle('user_id', ['type' => 'hidden', 'value' => 1]);
             echo $this->Form->control('title');
             echo $this->Form->control('body');
-            echo $this->Form->control('published');           
+            //echo $this->Form->control('published');           
             //echo $this->Form->control('tags._ids', ['options' => $tags]);
-            echo $this->Form->control('tag_string', ['type' => 'text']);
+            //echo $this->Form->control('tag_string', ['type' => 'text']);
         ?>
     </fieldset>
     <?= $this->Form->button(__('Submit')) ?>
