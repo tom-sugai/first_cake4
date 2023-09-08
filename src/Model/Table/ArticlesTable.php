909b4aad14d1ca03d2081d6932d8f358c6e2c06f
Tom Sugai <tom.sugai@gmail.com>
2023-09-08 12:18:41 +0900

    create simple slug beforsave

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -2,6 +2,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
@@ -14,7 +16,14 @@ class ArticlesTable extends Table
     public function initialize(array $config) : void
     {
         parent::initialize($config);
-
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
