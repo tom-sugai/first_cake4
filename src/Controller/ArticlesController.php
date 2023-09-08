664fe212a191cb662b08592fd9d61d1c955b8e54
Tom Sugai <tom.sugai@gmail.com>
2023-09-08 11:42:07 +0900

    made method  articles/add

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -9,6 +9,7 @@ class ArticlesController extends AppController
     {
         parent ::initialize();
         $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
     }
 
     public function index()
@@ -27,4 +28,27 @@ class ArticlesController extends AppController
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
     }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            
+            $article->user_id = 1;
+            // 変更: セッションから user_id をセット
+            //$article->user_id = $this->Auth->user('id');
+ 
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('The article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The article could not be saved. Please, try again.'));
+        }
+        $this->set('article', $article);
+        //$users = $this->Articles->Users->find('list', ['limit' => 200]);
+        //$tags = $this->Articles->Tags->find('list', ['limit' => 200]);
+        //$this->set(compact('article', 'users', 'tags'));
+    }
+
 }
