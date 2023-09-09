5c3b880a65e9c7c47830e4f5885d4a55c5da7af9
Tom Sugai <tom.sugai@gmail.com>
2023-09-09 13:56:16 +0900

    made method Articles/delete

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -76,4 +76,15 @@ class ArticlesController extends AppController
         $this->set(compact('article'));
         //$this->set(compact('article', 'users', 'tags'));
     }
+
+    public function delete($slug)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->Articles->delete($article)){
+            $this->Flash->success(__('The {0} article has been deleted.', $article->title));
+            return $this->redirect(['action' => 'index']);
+        }
+    }
 }
