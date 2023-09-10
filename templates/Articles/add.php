36b089fc2f4dcc5e4058a18d39cb8f4fff1c02ed
Tom Sugai <tom.sugai@gmail.com>
2023-09-10 10:43:29 +0900

    add Form->contorol('tags._ids,

--- templates/Articles/add.php
+++ templates/Articles/add.php
@@ -8,7 +8,7 @@
             echo $this->Form->control('title');
             echo $this->Form->control('body');
             //echo $this->Form->control('published');           
-            //echo $this->Form->control('tags._ids', ['options' => $tags]);
+            echo $this->Form->control('tags._ids', ['options' => $tags]);
             //echo $this->Form->control('tag_string', ['type' => 'text']);
         ?>
     </fieldset>
