e1a31ce9ac704ce07a8931a181bd769d78b510d2
Tom Sugai <tom.sugai@gmail.com>
2023-09-10 11:34:13 +0900

    add method findTagged

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -5,6 +5,7 @@ use Cake\ORM\Table;
 use Cake\Utility\Text;
 use Cake\Event\EventInterface;
 use Cake\Validation\Validator;
+use Cake\ORM\Query;
 
 class ArticlesTable extends Table
 {
@@ -34,12 +35,36 @@ class ArticlesTable extends Table
         return $validator;
     }
     
-
     public function beforSave(EventInterface $event, $entity, $options)
     {
         if ($entity->isNew() && !$entity->slug) {
             $sluggedTitle = Text::slug($entity->title);
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    public function findTagged(Query $query, array $options)
+    {
+        $columns = [
+            'Articles.id', 'Articles.user_id', 'Articles.title',
+            'Articles.body', 'Articles.published', 'Articles.created',
+            'Articles.slug',
+        ];
+
+        $query = $query
+            ->select($columns)
+            ->distinct($columns);
+
+        if (empty($options['tags'])) {
+            // タグが指定されていない場合は、タグのない記事を検索します。
+            $query->leftJoinWith('Tags')
+                ->where(['Tags.title IS' => null]);
+        } else {
+            // 提供されたタグが1つ以上ある記事を検索します。
+            $query->innerJoinWith('Tags')
+                ->where(['Tags.title IN' => $options['tags']]);
+        }
+
+        return $query->group(['Articles.id']); 
     }
 }
