30b587c9b1b0eadaf05f6054d26532bca447005d
Tom Sugai <tom.sugai@gmail.com>
2023-09-11 16:30:14 +0900

    add body rows 3

--- templates/Articles/add.php
+++ templates/Articles/add.php
@@ -6,12 +6,12 @@
             //echo $this->Form->control('user_id', ['options' => $users]);
             echo $this->Form->controle('user_id', ['type' => 'hidden', 'value' => 5]);
             echo $this->Form->control('title');
-            echo $this->Form->control('body');
+            echo $this->Form->control('body',['rows' => '3']);
             //echo $this->Form->control('published');           
             //echo $this->Form->control('tags._ids', ['options' => $tags]);
             //echo $this->Form->control('tag_string', ['type' => 'text']);
         ?>
     </fieldset>
-    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->button(__('Save Article')) ?>
     <?= $this->Form->end() ?>
 </div>
