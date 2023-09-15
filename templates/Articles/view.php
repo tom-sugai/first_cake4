3486c817d7d038965fc707b732894a2e0376763c
Tom Sugai <tom.sugai@gmail.com>
2023-09-15 11:21:10 +0900

    comment out 'display tag string'

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -1,7 +1,7 @@
 <p><?= "id : " . $article->id ?>
 <h1><?= h($article->title) ?></h1>
 <p><?= h($article->body) ?></p>
-// display tag_string
+<!-- // display tag_string -->
 <p><b>Tags:</b><?= h($article->tag_string) ?></p>
 <p><small>Created : <?= $article->created->format(DATE_RFC850) ?></small></p>
 <p><?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?></P>
