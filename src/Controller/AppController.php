82e48f1d46854332b2ae868a7aeeef0233e5f164
Tom Sugai <tom.sugai@gmail.com>
2023-09-15 10:01:24 +0900

    load the AuthorizationComponent

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -45,6 +45,8 @@ class AppController extends Controller
         $this->loadComponent('Flash');
         // 認証結果を確認し、サイトのロックを行うために次の行を追加します
         $this->loadComponent('Authentication.Authentication');
+        //
+        $this->loadComponent('Authorization.Authorization');
 
         /*
          * Enable the following component for recommended CakePHP form protection settings.
