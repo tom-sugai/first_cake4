3e4235df56d411ac3845f20c3be1688c31b2e01a
Tom Sugai <tom.sugai@gmail.com>
2023-09-23 14:05:26 +0900

    del skipAuthorization from login

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -22,7 +22,7 @@ class UsersController extends AppController
 
     public function login()
     {
-        $this->Authorization->skipAuthorization();
+        //$this->Authorization->skipAuthorization();
         $this->request->allowMethod(['get', 'post']);
         $result = $this->Authentication->getResult();
         // POST, GET を問わず、ユーザーがログインしている場合はリダイレクトします
