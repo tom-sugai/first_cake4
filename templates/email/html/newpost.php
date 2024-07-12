cf33908208e11d9e2f38eac9bff3fc99999c2771
Tom Sugai <tom.sugai@gmail.com>
2024-07-12 23:07:09 +0900

    email template newpost change to body link

--- templates/email/html/newpost.php
+++ templates/email/html/newpost.php
@@ -1,4 +1,4 @@
 <?= $message ?><br>
 <?= "投稿番号 : " . $article->id ?><br>
 <?= "タイトル : " . $article->title ?><br>
-<?= "本　　文 : " . $article->body ?><br>
+<?= "本　　文 : " . $this->Html->link($article->body, ['controller' => 'Articles', 'action' => 'view', $article->slug]) . "<br/>" ?><br>
